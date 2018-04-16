3c52252dd906788bfa00fe9b90af878c417b142e
Benjie <benjiecodes@gmail.com>
2018-04-16 15:40:49 -0400

    created comment reducer and mixed both posts and comments into one main index reducer

---
+++ client/reducers/comments.js
@@ -0,0 +1,12 @@
+// reducer is going to take two things:
+
+// 1. The action (information of what event happened)
+
+// 2. Copy of the current default object/state
+
+function comments(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default comments;
