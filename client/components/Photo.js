f60864d9b4e5d753b734e995b56c313002f0f03c
Benjie <benjiecodes@gmail.com>
2018-04-23 11:30:08 -0400

    added css transition group for likes

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import CSSTranstitionGroup from "react-addons-css-transition-group";
 
 class Photo extends React.Component {
   render() {
@@ -7,7 +8,23 @@ class Photo extends React.Component {
     return (
       <figure className="grid-figure">
         <div className="grid-photo-wrap">
-          <Link to={`/view/${post.code}`}>{post.caption}</Link>
+          <Link to={`/view/${post.code}`}>
+            <img
+              src={post.display_src}
+              alt={post.caption}
+              className="grid-photo"
+            />
+          </Link>
+
+          <CSSTranstitionGroup
+            transitionName="like"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}
+          >
+            <span key={post.likes} className="likes-heart">
+              {post.likes}
+            </span>
+          </CSSTranstitionGroup>
         </div>
       </figure>
     );
