ccefb878ba72cacfa72f753ecddef6a55a932d06
Benjie <benjiecodes@gmail.com>
2018-04-24 11:40:12 -0400

    added Photo component to the SinglePage component

--- client/components/SinglePage.js
+++ client/components/SinglePage.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Photo from "./Photo";
 import { render } from "react-router";
 
 class SinglePage extends React.Component {
   render() {
-    return <div className="single-photo">I'm the single page</div>;
+    // get index of post
+    const i = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+    console.log(post);
+    // get post
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+      </div>
+    );
   }
 }
 
